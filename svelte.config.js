fc37e4345033f75b5b1d8e80435895680611f051
Andrés Felipe Díaz Ossa <afdiazo@unincca.edu.co>
2026-03-31 20:11:58 -0500

    fix: use 404 fallback for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ fallback: 'index.html', strict: false }),
+		adapter: adapter({ fallback: '404.html', strict: false }),
 		paths: {
 			base: dev ? '' : '/ActividadGithub_CICD'
 		},
