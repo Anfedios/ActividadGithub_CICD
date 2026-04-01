228d50a2605598884fbb91eb64304b461101ae09
Andrés Felipe Díaz Ossa <afdiazo@unincca.edu.co>
2026-03-31 19:44:04 -0500

    chore: ci/cd github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/ActividadGithub_CICD'
+		},
+		prerender: {
+			entries: ['*']
+		}
 	}
 };
 
